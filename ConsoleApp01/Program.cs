6dd957d94699471e09fecf2154226cda77d9a5b3
vine86 <xieyehanta@gmail.com>
2023-03-01 00:23:42 +0300

    added actual program

--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace ConsoleApp01
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp01
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                try
+                {
+                    Console.Write("N: ");
+                    string str = Console.ReadLine();
+                    if (string.IsNullOrEmpty(str)) throw new FormatException("введено null.");
+                    if (str.Contains(',')) throw new FormatException("используйте точку вместо запятой.");
+                    if (!int.TryParse(str, out int n) || string.IsNullOrWhiteSpace(str) || n <= 0) throw new FormatException("пустой символ, буква или нецелое число, либо число не натуральное.");
+                    Console.WriteLine("Марсены: " + string.Join(", ", Marsen(n)));
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+        static List<int> Marsen(int n)
+        {
+            List<int> primes = new List<int>();
+            for (int i = 2; i <= n; i++)
+            {
+                bool prime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime) primes.Add(i);
+            }
+            List<int> marsens = new List<int>();
+            foreach (int i in primes)
+            {
+                int marsen = (int)Math.Pow(2, i) - 1;
+                if (primes.Contains(marsen)) marsens.Add(marsen);
+            }
+            return marsens;
         }
     }
 }
