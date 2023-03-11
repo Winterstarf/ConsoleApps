fc1d513ad60f3c8df9a262d5b736dd3bba07748d
vine86 <xieyehanta@gmail.com>
2023-03-11 16:39:33 +0300

    added actual program

--- ConsoleApp04/Program.cs
+++ ConsoleApp04/Program.cs
@@ -6,13 +6,23 @@ namespace ConsoleApp04
     {
         static void Main(string[] args)
         {
-            /*имеется аррей стрингов с фамами сотрудников
-             фамилии предопределены (иванов, петров, сидоров)
-            и массив с именами сотрудников (ваня, петя, коля)
-            вывести фам и имя сотрудника*/
-            string[] lastName = new string[3] { "Иванов", "Петров", "Сидоров" };
-            string[] firstName = new string[3] { "Ваня", "Петя", "Коля" };
-            for (int i = 0; i < lastName.Length; i++) Console.WriteLine($"{lastName[i]} {firstName[i]}");
+            /*Дан массив действительных чисел размерности N (пользователем вводится с клавиатуры и количество членнов последовательности, 
+            и сами члены последовательности). Определить, сколько в нем отрицательных, положительных и нулевых элементов.*/
+            Console.Write("N: ");
+            ArraySorter(Convert.ToInt32(Console.ReadLine()));
+        }   
+        static void ArraySorter(int n)
+        {
+            double[] nums = new double[n];
+            int cnt1 = 0, cnt2 = 0, cnt3 = 0; //-,+,0
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+                if (nums[i] < 0) cnt1++;
+                else if (nums[i] > 0) cnt2++;
+                else cnt3++;
+            }
+            Console.WriteLine($"Positives: {cnt2}, Negatives: {cnt1}, Zeroes {cnt3}");
         }
     }
 }
