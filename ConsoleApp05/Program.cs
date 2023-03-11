ea39d43fe232c963a020c79dd64ebbcf293d5cd6
vine86 <xieyehanta@gmail.com>
2023-03-11 16:50:01 +0300

    added consoleapp05 and actual prog

---
+++ ConsoleApp05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Дана последовательность чисел, среди которых имеется единственный нуль. Вывести все числа до нуля включительно.*/
+            Console.Write("N: ");
+            ArrayZeroFinder(Convert.ToInt32(Console.ReadLine()));
+        }
+        static void ArrayZeroFinder(int n)
+        {
+            double[] nums = new double[n];
+            for (int i = 0; i < nums.Length; i++) nums[i] = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) break;
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
