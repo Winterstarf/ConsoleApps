9ffd4947aa3a998fcca82678a017ff21cb1df93c
vine86 <95592792+vine86@users.noreply.github.com>
2023-03-15 15:30:35 +0300

    updated

--- ConsoleApp08/Program.cs
+++ ConsoleApp08/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp08
 {
@@ -6,22 +6,43 @@ namespace ConsoleApp08
     {
         static void Main(string[] args)
         {
-            /*дана матрица nxm, заполняется пользователем, в каждой строке этой матрицы найти мин и макс число*/
-            Console.Write("Строки: ");
+            Console.Write("Количество строк: ");
             int n = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Столбцы: ");
+            Console.Write("Количество столбцов: ");
             int m = Convert.ToInt32(Console.ReadLine());
             CustomMatrix(n, m);
         }
         static void CustomMatrix(int n, int m)
         {
-            int[,] nums = new int[n, m];
+            int[,] matrix = new int[n, m];
             for (int i = 0; i < n; i++)
             {
+                Console.WriteLine($"Введите элементы {i+1}-й строки:");
+                for (int j = 0; j < m; j++) matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+            MatrixSorter(matrix, n, m);
+
+        }
+        static void MatrixSorter(int[,] matrix, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int min = matrix[i, 0];
+                int max = matrix[i, 0];
                 for (int j = 0; j < m; j++)
                 {
-                    nums[i,j] = Convert.ToInt32(Console.ReadLine());
+                    if (matrix[i, j] < min)
+                    {
+                        min = matrix[i, j];
+                    }
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
                 }
+                Console.WriteLine($"Min {i+1}: {min}");
+                Console.WriteLine($"Маx {i+1}: {max}");
             }
         }
     }
