8c75fc122a00060168a879b94fc10800bb9e58a1
vine86 <xieyehanta@gmail.com>
2023-03-17 23:27:02 +0300

    added ConsoleApp09 and prog there

---
+++ ConsoleApp09/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConsoleApp08;
+
+namespace ConsoleApp09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = MatrixMethods.MatrixInput(n, n);
+            Console.WriteLine("Оригинальная матрица");
+            MatrixMethods.ShowMatrix(matrix);
+            Console.WriteLine($"След матрицы: {MatrixMethods.MatrixTrack(matrix)}");
+            Console.WriteLine("Матрица, чётные строки которой поделены на след");
+            MatrixMethods.ShowMatrix(MatrixMethods.MatrixEvenOddRowsTransform(matrix));
+        }
+    }
+}
