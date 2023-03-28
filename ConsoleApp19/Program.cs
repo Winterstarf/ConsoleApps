06ec45d80eb6c2c1b10f3325c8d7a1fa20326b07
vine86 <xieyehanta@gmail.com>
2023-03-29 01:00:31 +0300

    actual prog

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SquareMatrixApp;
 
 namespace ConsoleApp19
 {
@@ -6,7 +7,17 @@ namespace ConsoleApp19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("M: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = MatrixMethods.MatrixInput(n, m);
+            Console.WriteLine($"Оригинальная матрица: ");
+            MatrixMethods.ShowMatrix(matrix);
+
+            Console.WriteLine();
+            MatrixMethods.MatrixMinMax(matrix);
         }
     }
 }
