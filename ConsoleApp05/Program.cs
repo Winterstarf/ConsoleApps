cb404a360f3b580187fb00d44b7da056c3101aae
vine86 <xieyehanta@gmail.com>
2023-03-29 00:01:26 +0300

    fixed 5

--- ConsoleApp05/Program.cs
+++ ConsoleApp05/Program.cs
@@ -8,21 +8,27 @@ namespace ConsoleApp05
         {
             /*номер 5*/
             Console.Write("x: ");
-            Console.WriteLine(MyMethods.Factorial(6));
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Cos(x): {MyMethods.MyCos(x)}");
+            Console.WriteLine($"Проверка: {Math.Cos(x)}");
         }
     }
     public class MyMethods
     {
         public static double MyCos(double x)
         {
-            const int border = 100;
-            double cos = 1;
-            for (int i = 0; i < border; i++) cos = (Math.Pow(-1, i) * Math.Pow(x, 2 * i)) / MyMethods.Factorial(2 * i);
-            return cos;
+            double sum = 1;
+            double term = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                term *= (-1) * x * x / ((2 * i - 1) * 2 * i);
+                sum += term * 1;
+            }
+            return sum;
         }
-        public static double Factorial(int x)
+        public static long Factorial(int x)
         {
-            int fact = 1;
+            long fact = 1;
             for (int i = 1; i <= x; i++) fact *= i;
             return fact;
         }
