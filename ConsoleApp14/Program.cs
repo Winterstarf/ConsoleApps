43761d1bbddb57a3ba14e21bcdd8dd4161a5fb92
vine86 <95592792+vine86@users.noreply.github.com>
2023-03-28 16:53:43 +0300

    fixed Factorial method

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -23,7 +23,7 @@ namespace ConsoleApp14
         public static double Factorial(int x)
         {
             int fact = 1;
-            for (int i = 1; i < x; i++) fact *= i;
+            for (int i = 1; i <= x; i++) fact *= i;
             return fact;
         }
     }
