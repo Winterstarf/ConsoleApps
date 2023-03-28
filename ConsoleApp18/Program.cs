5ece2127805b34098e9c1a2d2e905cdf3adabc34
vine86 <xieyehanta@gmail.com>
2023-03-29 00:54:18 +0300

    added actual prog

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using MatrixMethods;
+using SquareMatrixApp;
 
 namespace ConsoleApp18
 {
@@ -7,11 +7,50 @@ namespace ConsoleApp18
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("N (exit чтобы закрыть): ");
+                string input = Console.ReadLine();
+                if (input == "exit") System.Environment.Exit(0);
+                if (!int.TryParse(input, out int n) || string.IsNullOrWhiteSpace(input) || n <= 0) throw new Exception("введено пустое или отрицательное число");
+
+                int[,] matrix = MatrixMethods.BinaryMatrix(n);
+                Console.WriteLine("Оригинальная матрица: ");
+                SquareMatrixApp.MatrixMethods.ShowMatrix(matrix);
+
+                int sum = MatrixMethods.BinaryMatrixSum(matrix, n);
+                Console.WriteLine($"Сумма элементов гл. диагонали: {sum}");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
-    class MyMethods : MatrixMethods.MatrixMethods
+    class MatrixMethods
     {
+        public static int[,] BinaryMatrix(int n) //возвращает квадратную матрицу с 1-ми по гл. и поб. диагоналях и 0-ми как остальные элементы
+        {
+            int[,] matrix = new int[n, n];
 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j || j == n - i - 1) matrix[i, j] = 1;
+                    else matrix[i, j] = 0;
+                }
+            }
+            
+            return matrix;
+        }
+        public static int BinaryMatrixSum(int[,] matrix, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) if (i == j) sum += matrix[i, j];
+            return sum;
+        }
     }
 }
