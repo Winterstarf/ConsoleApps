0f377ae779f7ed71954e6a4b49c71eb7e4fc7bb7
winterstarf <xieyehanta@gmail.com>
2023-06-14 19:49:50 +0300

    1

---
+++ src/ConsoleAppPractic4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPractic4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = Console.ReadLine();
+            int count = f.Count(char.IsPunctuation);
+            Console.WriteLine($"Знаков препинания - {count}");
+            Console.ReadKey();
+        }
+    }
+}
