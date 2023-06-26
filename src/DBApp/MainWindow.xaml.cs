522622add6b7b4f90652a924bcc60848ca92e1aa
Winterstarf <xieyehanta@gmail.com>
2023-06-26 11:50:51 +0300

    1

--- src/DBApp/MainWindow.xaml.cs
+++ src/DBApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
