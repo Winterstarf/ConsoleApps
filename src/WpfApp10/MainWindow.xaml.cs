3e6bc0b067672cc9e20dfbe999370451ae3697f5
student <student@104-07>
2023-10-11 15:57:13 +0300

    1

---
+++ src/WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Label label)
+            {
+                if (label.Content is Panel panel)
+                {
+                    panel.Children.Clear();
+                    panel.Children.Add(new Button { Content = "button1", HorizontalAlignment = HorizontalAlignment.Left, VerticalAlignment = VerticalAlignment.Top, Width = 30, Height = 10 });
+                    panel.Children.Add(new Button { Content = "button2", HorizontalAlignment = HorizontalAlignment.Left, VerticalAlignment = VerticalAlignment.Bottom, Width = 30, Height = 10 });
+                    panel.Children.Add(new Button { Content = "button3", HorizontalAlignment = HorizontalAlignment.Right, VerticalAlignment = VerticalAlignment.Top, Width = 30, Height = 10 });
+                    panel.Children.Add(new Button { Content = "button1", HorizontalAlignment = HorizontalAlignment.Right, VerticalAlignment = VerticalAlignment.Bottom, Width = 30, Height = 10 });
+                    /*if (e.OriginalSource is MouseButtonEventArgs args && args.ChangedButton == MouseButton.Left)
+                    {
+                        
+                    }*/
+                }
+            }    
+        }
+    }
+}
