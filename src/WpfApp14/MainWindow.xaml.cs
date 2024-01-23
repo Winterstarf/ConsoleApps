7303c154b6df8f41f9bde221e8f0a54ca9bbcbc8
uetktempp@hotmail.com <uetktempp@hotmail.com>
2024-01-23 14:51:26 +0300

    1

--- src/WpfApp14/MainWindow.xaml.cs
+++ src/WpfApp14/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -16,6 +15,26 @@ namespace WpfApp14
         public MainWindow()
         {
             InitializeComponent();
+
+            SolidColorBrush black = new SolidColorBrush(Colors.Black);
+            SolidColorBrush red = new SolidColorBrush(Colors.Red);
+
+            Line cl1 = new Line();
+            cl1.X1 = -3;
+            cl1.X2 = 3;
+            cl1.Y1 = 0;
+            cl1.Y2 = 0;
+            cl1.StrokeThickness = 1;
+            cl1.Stroke = black;
+            main_cvs.Children.Add(cl1);
+            Line cl2 = new Line();
+            cl2.X1 = 0;
+            cl2.X2 = 0;
+            cl2.Y1 = -3;
+            cl2.Y2 = 3;
+            cl2.StrokeThickness = 1;
+            cl2.Stroke = black;
+            main_cvs.Children.Add(cl2);
         }
 
         private void main_cvs_MouseDown(object sender, MouseButtonEventArgs e)
@@ -30,30 +49,47 @@ namespace WpfApp14
                 main_cvs.Children.Clear();
                 res_tb.Text = "";
 
+                SolidColorBrush black = new SolidColorBrush(Colors.Black);
+                SolidColorBrush red = new SolidColorBrush(Colors.Red);
+
+                Line cl1 = new Line();
+                cl1.X1 = -3;
+                cl1.X2 = 3;
+                cl1.Y1 = 0;
+                cl1.Y2 = 0;
+                cl1.StrokeThickness = 1;
+                cl1.Stroke = black;
+                main_cvs.Children.Add(cl1);
+                Line cl2 = new Line();
+                cl2.X1 = 0;
+                cl2.X2 = 0;
+                cl2.Y1 = -3;
+                cl2.Y2 = 3;
+                cl2.StrokeThickness = 1;
+                cl2.Stroke = black;
+                main_cvs.Children.Add(cl2);
+
                 if (double.Parse(ax_tb.Text) > 250 || double.Parse(ax_tb.Text) < -250 || !double.TryParse(ax_tb.Text, out double ax) || double.Parse(ay_tb.Text) > 150 || double.Parse(ay_tb.Text) < -150 || !double.TryParse(ay_tb.Text, out double ay))
                 {
-                    throw new Exception($"Значение точки a превышает лимиты или не является числом");
+                    throw new Exception($"Значение точки a превышает лимиты или не является числом.");
                 }
                 if (double.Parse(bx_tb.Text) > 250 || double.Parse(bx_tb.Text) < -250 || !double.TryParse(bx_tb.Text, out double bx) || double.Parse(by_tb.Text) > 150 || double.Parse(by_tb.Text) < -150 || !double.TryParse(by_tb.Text, out double by))
                 {
-                    throw new Exception($"Значение точки b превышает лимиты или не является числом");
+                    throw new Exception($"Значение точки b превышает лимиты или не является числом.");
                 }
                 if (double.Parse(cx_tb.Text) > 250 || double.Parse(cx_tb.Text) < -250 || !double.TryParse(cx_tb.Text, out double cx) || double.Parse(cy_tb.Text) > 150 || double.Parse(cy_tb.Text) < -150 || !double.TryParse(cy_tb.Text, out double cy))
                 {
-                    throw new Exception($"Значение точки c превышает лимиты или не является числом");
+                    throw new Exception($"Значение точки c превышает лимиты или не является числом.");
                 }
                 if (double.Parse(dx_tb.Text) > 250 || double.Parse(dx_tb.Text) < -250 || !double.TryParse(dx_tb.Text, out double dx) || double.Parse(dy_tb.Text) > 150 || double.Parse(dy_tb.Text) < -150 || !double.TryParse(dy_tb.Text, out double dy))
                 {
-                    throw new Exception($"Значение точки d превышает лимиты или не является числом");
+                    throw new Exception($"Значение точки d превышает лимиты или не является числом.");
                 }
 
                 Point a = new Point(ax, ay);
                 Point b = new Point(bx, by);
                 Point c = new Point(cx, cy);
                 Point d = new Point(dx, dy);
-
-                SolidColorBrush black = new SolidColorBrush(Colors.Black);
-                SolidColorBrush red = new SolidColorBrush(Colors.Red);
 
                 Line ab = new Line();
                 ab.X1 = a.X;
@@ -63,7 +99,6 @@ namespace WpfApp14
                 ab.StrokeThickness = 2;
                 ab.Stroke = black;
                 main_cvs.Children.Add(ab);
-
                 Line cd = new Line();
                 cd.X1 = c.X;
                 cd.X2 = d.X;
@@ -73,53 +108,54 @@ namespace WpfApp14
                 cd.Stroke = red;
                 main_cvs.Children.Add(cd);
 
-                Point ta = new Point(ab.X1, ab.Y1);
-                Point tb = new Point(ab.X2, ab.Y2);
-                Point tc = new Point(cd.X1, cd.Y1);
-                Point td = new Point(cd.X2, cd.Y2);
-
                 //метод через формулу углового коэффа, k = (y2 - y1) / (x2 - x1)
-                double k1 = (tb.Y - ta.Y) / (tb.X - ta.X);
-                double k2 = (td.Y - tc.Y) / (td.X - tc.X);
+                double k1 = (b.Y - a.Y) / (b.X - a.X);
+                double k2 = (d.Y - c.Y) / (d.X - c.X);
 
-                //b уравнений прямых
-                double b1 = ta.Y - k1 * ta.X;
-                double b2 = tc.Y - k2 * tc.X;
+                //b уравнений прямых (смещение)
+                double b1 = a.Y - k1 * a.X;
+                double b2 = c.Y - k2 * c.X;
 
                 //x и y точки пересечения по коэффам
                 double ix = (b2 - b1) / (k1 - k2);
                 double iy = k1 * ix + b1;
 
                 //расстояние между точками
-                double dist1 = Math.Sqrt(Math.Pow(tb.X - ta.X, 2) + Math.Pow(tb.Y - ta.Y, 2));
-                double dist2 = Math.Sqrt(Math.Pow(td.X - tc.X, 2) + Math.Pow(td.Y - tc.Y, 2));
+                double dist1 = Math.Sqrt(Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2));
+                double dist2 = Math.Sqrt(Math.Pow(d.X - c.X, 2) + Math.Pow(d.Y - c.Y, 2));
 
-                if (k1 == k2)
-                {
-                    res_tb.Text = "Прямые параллельны";
-                }
+                string answer = $"Точка пер.в ({ Math.Round(ix, 2)}, { Math.Round(iy, 2)})";
 
+                //kostyly ktore dzialaja, nie wiem jak
+                if (k1 == k2) answer = "Прямые параллельны";
                 else if (double.IsInfinity(k1) == true && k2 == 0)
                 {
-                    ix = ta.X;
+                    ix = a.X;
                     iy = b2;
-
-                    res_tb.Text = $"Т. пер. в ({ix}, {iy}). Прямые перпедикулярны";
+                    answer += $". Прямые перпедикулярны";
                 }
-
-                else
+                else if (double.IsInfinity(k2) == true || double.IsInfinity(b2) == true)
                 {
+                    ix = c.X;
+                    iy = b1;
+                    answer += $". Прямые перпедикулярны";
+                }
+                else if (double.IsInfinity(k1) == true && double.IsInfinity(b1) == true)
+                {
+                    k1 = 0;
+                    b1 = 0;
                     ix = (b2 - b1) / (k1 - k2);
                     iy = k1 * ix + b1;
-
-                    res_tb.Text = $"Т. пер. в ({ix}, {iy}). Прямые пересекаются";
+                    answer += $". Прямые пересекаются";
                 }
+                else answer += $". Прямые пересекаются";
 
-                res_tb.Text += $". Расстояние между a и b, с и d = {dist1}, {dist2}";
+                answer += $". Расстояние между точками a и b = {Math.Round(dist1, 2)}, между c и d = {Math.Round(dist2, 2)}.";
+                res_tb.Text = answer;
             }
             catch (FormatException)
             {
-                MessageBox.Show("Одно/несколько из введённых значений - не число или пусто");
+                MessageBox.Show("Одно/несколько из введённых значений - не число или пусто.");
             }
             catch (Exception ex)
             {
